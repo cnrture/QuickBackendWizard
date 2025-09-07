714f81ec33deb92d917dbdf15649ba88697e324c
candroid <cnrture@gmail.com>
2025-09-08 02:18:51 +0300

    Refactor getGroupIcon method to use expression body syntax

--- src/main/kotlin/com/github/cnrture/quickbackendwizard/generators/QBWProjectTemplatesFactory.kt
+++ src/main/kotlin/com/github/cnrture/quickbackendwizard/generators/QBWProjectTemplatesFactory.kt
@@ -4,15 +4,12 @@ import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.util.IconLoader
 import com.intellij.platform.ProjectTemplate
 import com.intellij.platform.ProjectTemplatesFactory
-import javax.swing.Icon
 
 class QBWProjectTemplatesFactory : ProjectTemplatesFactory() {
 
     override fun getGroups(): Array<String> = arrayOf("Quick Backend Wizard")
 
-    override fun getGroupIcon(group: String?): Icon? {
-        return IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
-    }
+    override fun getGroupIcon(group: String?) = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
 
     override fun createTemplates(group: String?, context: WizardContext): Array<ProjectTemplate> {
         return arrayOf(QBWSpringBootProjectTemplate())
