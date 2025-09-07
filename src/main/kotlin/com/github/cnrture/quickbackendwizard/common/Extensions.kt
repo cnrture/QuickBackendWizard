537b9f2987f3118e7e551ce6fde0af80a0158ba9
candroid <cnrture@gmail.com>
2025-09-07 22:51:31 +0300

    Add utility functions for displaying notifications

---
+++ src/main/kotlin/com/github/cnrture/quickbackendwizard/common/Extensions.kt
@@ -0,0 +1,36 @@
+package com.github.cnrture.quickbackendwizard.common
+
+import com.intellij.ide.BrowserUtil
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.util.IconLoader
+
+object Utils {
+    fun showInfo(title: String? = null, message: String, type: NotificationType = NotificationType.INFORMATION) {
+        val notification = NotificationGroupManager.getInstance()
+            .getNotificationGroup("QuickBackendWizard")
+            .createNotification(
+                title = title ?: "Quick Backend Wizard",
+                content = message,
+                type = type,
+            )
+        notification.addAction(
+            object : AnAction("Contact Developer") {
+                override fun actionPerformed(e: AnActionEvent) {
+                    BrowserUtil.browse("https://candroid.dev/")
+                }
+            }
+        )
+        notification.addAction(
+            object : AnAction("Open Plugin Page") {
+                override fun actionPerformed(e: AnActionEvent) {
+                    BrowserUtil.browse("https://quickprojectwizard.candroid.dev/overview")
+                }
+            }
+        )
+        notification.icon = IconLoader.getIcon("/META-INF/pluginIcon.svg", this::class.java)
+        notification.notify(null)
+    }
+}
