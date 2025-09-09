a36a7815c0c95ccb26406254dd693e2fb46fd7fc
candroid <cnrture@gmail.com>
2025-09-09 05:21:12 +0300

    Remove unused import for QBWCheckbox in ProjectInfoContent

--- src/main/kotlin/com/github/cnrture/quickbackendwizard/generators/ProjectInfoContent.kt
+++ src/main/kotlin/com/github/cnrture/quickbackendwizard/generators/ProjectInfoContent.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.github.cnrture.quickbackendwizard.components.QBWCheckbox
 import com.github.cnrture.quickbackendwizard.components.QBWText
 import com.github.cnrture.quickbackendwizard.components.QBWTextField
 import com.github.cnrture.quickbackendwizard.theme.QBWTheme
